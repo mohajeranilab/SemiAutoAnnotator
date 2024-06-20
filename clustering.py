cd9db236cd5e373d185523483a173e4b8bea2db0
Kandage <thasanka.k@hotmail.com>
2024-06-20 10:40:10 -0600

    updated clustering file

--- clustering.py
+++ clustering.py
@@ -115,7 +115,7 @@ def plot_reduced_feature_space(features_tsne):
     plt.show()
 
 
-def cluster_and_plot(features_tsne, epsilon, min_samples, images_per_cluster, image_paths):
+def cluster_and_plot(features_tsne, epsilon, min_samples, image_paths, image_dir):
 
     X_tsne = features_tsne[:, 1:].astype(float)
     db = DBSCAN(eps=epsilon, min_samples=min_samples).fit(X_tsne)
@@ -129,91 +129,90 @@ def cluster_and_plot(features_tsne, epsilon, min_samples, images_per_cluster, im
     print("Estimated number of clusters: %d" % n_clusters)
     print("Estimated number of noise points: %d" % n_noise)
 
+    cluster_amount =500
     plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=labels, cmap='viridis')
     cluster_centers = {}
     cluster_diversity = {}
     cluster_closest_images = {}
-
+    used_indices = set()
     for cluster_label in set(labels):
-
         if cluster_label != -1:  # Ignore noise points
             cluster_points = X_tsne[labels == cluster_label]
             cluster_center = np.mean(cluster_points, axis=0)
             cluster_centers[cluster_label] = cluster_center
             plt.text(cluster_center[0], cluster_center[1], str(cluster_label),
-                     color='red', fontsize=12, weight='bold')
-            
+                    color='red', fontsize=12, weight='bold')
 
-            closest_image_index = pairwise_distances_argmin_min(cluster_center.reshape(1, -1), cluster_points)[0][0]
+
+        #     # Compute distances to the cluster center
+        #     #linalg.norm calculate euclidean distance (norm)
+        #     #The axis=1 parameter specifies that the norm should be calculated along each row (point) of the array.
+        #     #This results in a 1D array of shape (n,), where each element is the Euclidean distance from a point in cluster_points to cluster_center.
+            distances = np.linalg.norm(cluster_points - cluster_center, axis=1)
+             #     # np.argsort returns the indices that would sort an array.
+        #     # For the distances array, it returns the indices of the elements in ascending order.
+            closest_indices = np.argsort(distances)
+
+            # Ensure uniqueness
+            unique_closest_indices = []
+            for idx in closest_indices:
+                if len(unique_closest_indices) >= cluster_amount:
+                    break
+                absolute_idx = np.where(labels == cluster_label)[0][idx]  # Get the absolute index
+                if absolute_idx not in used_indices:
+                    unique_closest_indices.append(absolute_idx)
+                    used_indices.add(absolute_idx)
+
+            print(unique_closest_indices)
+            closest_image_paths = [image_paths[idx] for idx in unique_closest_indices]
+            cluster_closest_images[cluster_label] = closest_image_paths
+
         
-            closest_image_path = image_paths[closest_image_index]
-            cluster_closest_images[cluster_label] = closest_image_path
+       
 
-            cluster_size = len(cluster_points)
-            cluster_dispersion = np.std(np.linalg.norm(cluster_points - cluster_center, axis=1))
-            cluster_diversity[cluster_label] = (cluster_size, cluster_dispersion)
-
+       
+    for cluster_label, cluster_images in cluster_closest_images.items():
+        print("closest_images_cluster", cluster_label, len(cluster_images))
     plt.title('DBSCAN Clustering after t-SNE')
     plt.xlabel('t-SNE Component 1')
     plt.ylabel('t-SNE Component 2')
     plt.show()
 
     cluster_images = {cluster_label: [] for cluster_label in set(labels)}
-    cluster_centroids_dir = "clusters/cluster_centroids"
      
-    for i, label in enumerate(labels):
-        if label != -1:  # Ignore noise points
-            cluster_images[label].append(IMAGE_PATHS[i])
 
-    # Sort clusters by diversity (size * dispersion)
-    # sorted by worst to best diversity 
-    sorted_clusters = sorted(cluster_diversity.items(), key=lambda x: x[1][0] * x[1][1], reverse=True)
-    diverse_clusters = [(cluster[0], len(cluster_images[cluster[0]])) for cluster in sorted_clusters]
-    print(diverse_clusters)
-
-    clusters_dir = "clusters"
+    clusters_dir = f"{image_dir.parent}/clusters/"
     shutil.rmtree(clusters_dir, ignore_errors=True)
     os.makedirs(clusters_dir, exist_ok=True)
     
 
-    for cluster_label in set(labels):
 
-        if cluster_label != -1:  
-            cluster_num_dir = os.path.join(clusters_dir, f"cluster_{cluster_label}")
-            os.makedirs(cluster_num_dir, exist_ok=True)
-        
-
-
-            for i, label in enumerate(labels):
-                if label == cluster_label:
-                    image_path = IMAGE_PATHS[i]
-                    shutil.copy(image_path, cluster_num_dir)
-
-    for centroid_label, centroid_image  in cluster_closest_images.items():
-        print(centroid_image)
-        centroid_dir = os.path.join(cluster_centroids_dir, f"cluster_{centroid_label}")
+    for centroid_label, centroid_images  in cluster_closest_images.items():
+   
+        centroid_dir = os.path.join(clusters_dir, f"cluster_{centroid_label}/")
         os.makedirs(centroid_dir, exist_ok=True)
-        shutil.copy(str(centroid_image), centroid_dir)
+        for centroid_image in centroid_images:
+            shutil.copy(str(centroid_image), centroid_dir)
 
-    plt.bar(list(cluster_images.keys()), list(len(values) for values in cluster_images.values()), color="maroon", width=0.4)
-    plt.title("Images per cluster")
-    plt.xlabel('Cluster #')
-    plt.ylabel('# of images')
-    plt.show()
+    # plt.bar(list(cluster_images.keys()), list(len(values) for values in cluster_images.values()), color="maroon", width=0.4)
+    # plt.title("Images per cluster")
+    # plt.xlabel('Cluster #')
+    # plt.ylabel('# of images')
+    # plt.show()
      
-    for cluster_label, images in cluster_images.items():
+    # for cluster_label, images in cluster_images.items():
 
-        if cluster_label != -1:
-            num_images = min(len(images), images_per_cluster)
-            fig, axes = plt.subplots(1, num_images, figsize=(15, 3))
-            fig.suptitle(f"Images in {cluster_label}", fontsize=16)
+    #     if cluster_label != -1:
+    #         num_images = min(len(images), cluster_amount)
+    #         fig, axes = plt.subplots(1, num_images, figsize=(15, 3))
+    #         fig.suptitle(f"Images in {cluster_label}", fontsize=16)
 
-            for i in range(num_images):
-                image = plt.imread(images[i])
-                axes[i].imshow(image, cmap="gray")
-                axes[i].axis('off')
+    #         for i in range(num_images):
+    #             image = plt.imread(images[i])
+    #             axes[i].imshow(image, cmap="gray")
+    #             axes[i].axis('off')
     
-    plt.show()
+    # plt.show()
     
     return cluster_centers
 
@@ -225,28 +224,25 @@ def export_features(features_dict, filename):
 def main(image_dir, model_path):
     global IMAGE_PATHS
     # MODEL_PATH = filedialog.askopenfilename(initialdir="/", title="SELECT MODEL/WEIGHTS FILE")
-    # print(MODEL_PATH)
-    # # MODEL_PATH = Path("runs/detect/train37/weights/best.pt")
+    # MODEL_PATH = Path("runs/detect/train37/weights/best.pt")
 
     # IMAGE_FOLDER = filedialog.askdirectory(initialdir="/", title="SELECT IMAGE FOLDER")
-
-    # Now IMAGE_FOLDER contains the path to the selected folder containing images
-    # print("Selected Image Folder:", IMAGE_FOLDER)
     # IMAGE_DIR = Path(IMAGE_FOLDER)
-
-    IMAGE_PATHS = sorted(image_dir.glob("*.jpg")) # change to *.jpg for jpg images
-
+ 
     # INCORRECT_IMAGE_DIR = Path("incorrectly_detected")
     # INCORRECT_IMAGE_PATHS = sorted(INCORRECT_IMAGE_DIR.glob("*.png"))
+    IMAGE_PATHS = sorted(image_dir.glob("*.jpg")) # change to *.jpg for jpg images
+  
 
     LAYER_INDEX = 21 # 21 is the last index before detection head, choosing highest layer for high level features 
     SAMPLE_PERCANTAGE = 1
-    IMAGES_PER_CLUSTERS = 5
+
+    #IMAGES_PER_CLUSTERS = 5
 
     # t-SNE params
     # GOOD PARAMS: (~1000 images: (15, 5), (20, 5)), (~2000 images: (30, 5), )
     # new dataaset ~900 (20, 5), ~ 1800 (20, 5)
-    PERPLEXITY = 40 # controls number of neighbors used in the algorithm, a guess about the number of close neigbors each point has
+    PERPLEXITY = 20 # controls number of neighbors used in the algorithm, a guess about the number of close neigbors each point has
 
     # It determines the balance between local and global aspects of the data manifold. Higher perplexity values tend to result in more global views of the data, while lower perplexity values focus more on local structure.
 
@@ -266,8 +262,8 @@ def main(image_dir, model_path):
     print("Grabbing image features.....")
     features = create_features_list(model, IMAGE_PATHS, LAYER_INDEX, SAMPLE_PERCANTAGE)
 
-    print("Plotting image features")
-    plot_features_space(features)
+    # print("Plotting image features")
+    # plot_features_space(features)
 
     print("number of images:", int(len(IMAGE_PATHS) * SAMPLE_PERCANTAGE))
     print("Applying t-SNE.....")
@@ -276,9 +272,9 @@ def main(image_dir, model_path):
     print("Plotting transformed feature space.....")
     plot_reduced_feature_space(features_tsne)
 
-    cluster_centers = cluster_and_plot(features_tsne, EPSILON, MIN_SAMPLES, IMAGES_PER_CLUSTERS, IMAGE_PATHS)
+    cluster_centers = cluster_and_plot(features_tsne, EPSILON, MIN_SAMPLES, IMAGE_PATHS, image_dir)
 
-
+    export_features(features, f"{image_dir.parent}/features_list.npy")
     # incorrect_img_features = create_features_list(model, INCORRECT_IMAGE_PATHS, LAYER_INDEX, sample_percentage=0.15)
     # print("Matching images to clusters.....")
 
@@ -321,6 +317,5 @@ def main(image_dir, model_path):
     #         axes[i].set_title(image_paths[i].name)  # Set title as the image filename
 
     #     plt.show()       
-    # # # export_features(features, 'features_list.npy')
 if __name__ == "__main__":
     main()
